95c2afe48505bc241cfad6fda43d7f917482e7de
dat.ng31 <109670790+datnguyen31@users.noreply.github.com>
2024-08-14 22:33:04 +0700

    mission: add time to mission sample

--- mission_sample/mission_sample.cpp
+++ mission_sample/mission_sample.cpp
@@ -1,3 +1,4 @@
+#include "mission_sample.hpp"
 #include "../ps/inc/platform_services.hpp"
 
 #include <iostream>
@@ -7,17 +8,24 @@ using namespace PlatformServices;
 
 int main()
 {
+    // Create an instance of the MissionExecutive
     Executive MissionExecutive;
 
+    // Create an instance of the TimeService
+    TimeService& MissionTime = TimeService::getInstance();
+    MissionTime.setEpoch(MISSION_SAMPLE_EPOCH_YEAR, MISSION_SAMPLE_EPOCH_MONTH,
+                         MISSION_SAMPLE_EPOCH_DAY, MISSION_SAMPLE_EPOCH_HOUR,
+                         MISSION_SAMPLE_EPOCH_MINUTE, MISSION_SAMPLE_EPOCH_SECOND);
+
     // Load thread information from file
-    if(!MissionExecutive.loadThreadsFromFile("mission_modules.txt"))
+    if (!MissionExecutive.loadThreadsFromFile("mission_modules.txt"))
     {
         std::cerr << "Failed to load threads from file" << std::endl;
         return 0;
     }
 
     // Create and start both threads
-    if(MissionExecutive.create("SampleModule"))
+    if (MissionExecutive.create("SampleModule"))
     {
         std::cout << "Thread1 created successfully" << std::endl;
     }
@@ -26,7 +34,7 @@ int main()
         std::cerr << "Failed to create Thread1" << std::endl;
     }
 
-    while(1)
+    while (1)
     {
         std::this_thread::sleep_for(std::chrono::seconds(1));
     }
---
+++ mission_sample/mission_sample.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+/**
+ * @brief Version of the mission sample
+ */
+#define MISSION_SAMPLE_MAJOR_VERSION 1
+#define MISSION_SAMPLE_MINOR_VERSION 1
+#define MISSION_SAMPLE_LOCAL_VERSION 0
+
+/**
+ * @brief Epoch of the mission sample
+ */
+#define MISSION_SAMPLE_EPOCH_YEAR 2024
+#define MISSION_SAMPLE_EPOCH_MONTH 31
+#define MISSION_SAMPLE_EPOCH_DAY 10
+#define MISSION_SAMPLE_EPOCH_HOUR 0
+#define MISSION_SAMPLE_EPOCH_MINUTE 0
+#define MISSION_SAMPLE_EPOCH_SECOND 0
