6e6e8c8d0276b813e1a9764205c21fe3b3b53e35
dat.ng31 <109670790+datnguyen31@users.noreply.github.com>
2024-08-15 21:37:58 +0700

    mission: add jose mission, with temporary module - laboratory

---
+++ mission_jose/mission_jose.cpp
@@ -0,0 +1,45 @@
+#include "mission_jose.hpp"
+#include "../ps/inc/platform_services.hpp"
+
+#include <iostream>
+#include <thread>
+
+using namespace PlatformServices;
+
+int main()
+{
+    // Create an instance of the MissionExecutive
+    Executive MissionExecutive;
+
+    // Create an instance of the TimeService
+    TimeService& MissionTime = TimeService::getInstance();
+    MissionTime.setEpoch(MISSION_JOSE_EPOCH_YEAR, MISSION_JOSE_EPOCH_MONTH,
+                         MISSION_JOSE_EPOCH_DAY, MISSION_JOSE_EPOCH_HOUR,
+                         MISSION_JOSE_EPOCH_MINUTE, MISSION_JOSE_EPOCH_SECOND);
+
+    // Load thread information from file
+    if (!MissionExecutive.loadThreadsFromFile(MISSION_JOSE_THREAD_INFO))
+    {
+        std::cerr << "Failed to load threads from file" << std::endl;
+        return 0;
+    }
+
+    if(MissionExecutive.createAll())
+    {
+        std::cout << "All threads created successfully" << std::endl;
+    }
+    else
+    {
+        std::cerr << "Failed to create all threads" << std::endl;
+    }
+
+    while (1)
+    {
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+    }
+
+    // Clean up
+    MissionExecutive.destroyAll();
+
+    return 0;
+}
---
+++ mission_jose/mission_jose.hpp
@@ -0,0 +1,23 @@
+#pragma once
+
+/**
+ * @brief File with thread information
+ */
+#define MISSION_JOSE_THREAD_INFO "mission_modules.txt"
+
+/**
+ * @brief Version of the mission jose
+ */
+#define MISSION_JOSE_MAJOR_VERSION 1
+#define MISSION_JOSE_MINOR_VERSION 1
+#define MISSION_JOSE_LOCAL_VERSION 0
+
+/**
+ * @brief Epoch of the mission jose
+ */
+#define MISSION_JOSE_EPOCH_YEAR 2024
+#define MISSION_JOSE_EPOCH_MONTH 31
+#define MISSION_JOSE_EPOCH_DAY 10
+#define MISSION_JOSE_EPOCH_HOUR 0
+#define MISSION_JOSE_EPOCH_MINUTE 0
+#define MISSION_JOSE_EPOCH_SECOND 0
