66628de800392f132d192f47932ccd7edf8aa24a
dat.ng31 <109670790+datnguyen31@users.noreply.github.com>
2024-10-17 00:09:17 +0700

    update: 	- ps: new commit for module maker 	- mission: update jose with module maker

--- mission_jose/component_list.cmake
+++ mission_jose/component_list.cmake
@@ -2,7 +2,7 @@
 set(TARGET_DRIVER_LIST math linuxI2C bno055 navigation)
 
 # Define the list of platform services
-set(PLATFORM_SERVICE_LIST pes pms pts)
+set(PLATFORM_SERVICE_LIST pes pms pts pmm)
 
 # Define the list of modules (not used in the provided CMakeLists.txt)
 set(MODULE_LIST laboratory)
