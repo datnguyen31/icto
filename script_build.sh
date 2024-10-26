30a8e727d9af609eec8e1c3c948f821929e7d277
dat.ng31 <109670790+datnguyen31@users.noreply.github.com>
2024-10-26 23:42:31 +0700

    update: 	- new snapshot of driver 		+ update opencv camera 	- new snapshot of jnav 		+ compatible with new driver 	- update build script

--- script_build.sh
+++ script_build.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
 
 # Check if the correct number of arguments are provided
-if [ "$#" -ne 2 ]; then
+if [ "$#" -le 1 ]; then
     echo "Please choose a mission directory and target"
     exit 1
 fi
 
 MISSION=$1
 TARGET=$2
+JOBS=${3:-2}  # Set JOBS to 2 if not provided
+CLEAN=$4
+
 echo "Building: $MISSION"
 echo "Target: $TARGET"
+echo "Jobs: $JOBS"
 
-make clean
-make MISSION=$MISSION TOOLCHAIN=$TARGET -j2 install
+# Check if CLEAN is declared and not empty
+if [ -n "$CLEAN" ]; then
+    make clean
+fi
+
+make MISSION=$MISSION TOOLCHAIN=$TARGET -j$JOBS install
